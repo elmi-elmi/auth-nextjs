348f29dbbe12a0ee859583ce5a2dff03d4ad6459
elmi <shahrokhelmi@gmail.com>
2025-10-08 16:30:06 +0330

    fix/ fix svg error

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig: NextConfig = {
                 hostname: "dummyjson.com",
             },
         ],
+        dangerouslyAllowSVG: true,
+        contentDispositionType: 'attachment',
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
 };
 
